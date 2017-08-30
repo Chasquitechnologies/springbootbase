8b8507d6ea9b04f48e5231a270a6d9561729bee0
Luis Mego <lmego@chasquitechnologies.com>
2017-08-29 22:39:28 -0700

    feat - add in memory authentication and authorization

--- springbootbase/src/main/java/com/chasquitech/base/controller/ProductController.java
+++ springbootbase/src/main/java/com/chasquitech/base/controller/ProductController.java
@@ -62,5 +62,10 @@ public class ProductController {
         productService.deleteProduct(id);
         return "redirect:/products";
     }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(){
+         return "login";
+    }
 }
 
